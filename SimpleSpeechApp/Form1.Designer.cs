72e384583e47a5698451b8cafdf7ac42144473ef
yasuto-dev <yasutomisono@gmail.com>
2022-09-19 13:03:25 +0900

    プロジェクト ファイルを追加します。

---
+++ SimpleSpeechApp/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SimpleSpeechApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.sample_btn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ConnectDB = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DataView = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(521, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(521, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(521, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // sample_btn
+            // 
+            this.sample_btn.Location = new System.Drawing.Point(78, 130);
+            this.sample_btn.Name = "sample_btn";
+            this.sample_btn.Size = new System.Drawing.Size(107, 43);
+            this.sample_btn.TabIndex = 1;
+            this.sample_btn.Text = "Register";
+            this.sample_btn.UseVisualStyleBackColor = true;
+            this.sample_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(78, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // ConnectDB
+            // 
+            this.ConnectDB.Location = new System.Drawing.Point(87, 299);
+            this.ConnectDB.Name = "ConnectDB";
+            this.ConnectDB.Size = new System.Drawing.Size(107, 41);
+            this.ConnectDB.TabIndex = 6;
+            this.ConnectDB.Text = "ConnectDB";
+            this.ConnectDB.UseVisualStyleBackColor = true;
+            this.ConnectDB.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(265, 299);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(245, 107);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // DataView
+            // 
+            this.DataView.Location = new System.Drawing.Point(87, 364);
+            this.DataView.Name = "DataView";
+            this.DataView.Size = new System.Drawing.Size(107, 42);
+            this.DataView.TabIndex = 8;
+            this.DataView.Text = "DataView";
+            this.DataView.UseVisualStyleBackColor = true;
+            this.DataView.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataView);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ConnectDB);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sample_btn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Simple_speech_app";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button sample_btn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button ConnectDB;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button DataView;
+    }
+}
+
---
+++ SimpleSpeechApp/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Synthesis;
+using MySql.Data.MySqlClient;
+
+namespace SimpleSpeechApp
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void Speech(string LabelName)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.  
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+
+            // Speak a string.  
+            synth.Speak(LabelName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //buttonをさがす。子コントロールは検索しない。
+
+            Control c1 = this.Controls["button1"];
+            Control c2 = this.Controls["button2"];
+            Control c3 = this.Controls["button3"];
+
+            string Input_Text = textBox1.Text;
+
+            //button1が見つかれば、Textを変更する
+            if (c1 != null)
+            {
+                button1.Name = Input_Text;
+                button1.Text = Input_Text;
+            }
+            //button2が見つかれば、Textを変更する
+            else if (c2 != null)
+            {
+                button2.Name = Input_Text;
+                button2.Text = Input_Text;
+            }
+            //button3が見つかれば、Textを変更する
+            else if (c3 != null)
+            {
+                button3.Name = Input_Text;
+                button3.Text = Input_Text;
+
+            }
+        }
+
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string LabelName = ((Button)sender).Text;
+
+            // MessageBox.Show(LabelName);
+
+
+            Speech(LabelName);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+            {
+            string LabelName = ((Button)sender).Text;
+
+            Speech(LabelName);
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string LabelName = ((Button)sender).Text;
+
+            Speech(LabelName);
+
+        }
+
+
+
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            List<string> Btnlist = new List<string> { };
+
+            for (int i = 1; i <= 3; i++)
+            {
+
+            }
+
+
+            button1.Text = "button1";
+            button1.Name = "button1";
+            button2.Text = "button2";
+            button2.Name = "button2"; 
+            button3.Text = "button3";
+            button3.Name = "button3";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string sLogin = "server=localhost; database = simpledata; userid=root; password=root; ";
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(sLogin);
+                connection.Open();
+                MessageBox.Show("接続しました");
+                connection.Close();
+            }
+            catch (MySqlException me)
+            {
+                MessageBox.Show("ERROR: " + me.Message);
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            string sLogin = "server=localhost; database = simpledata; userid=root; password=root; ";
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(sLogin);
+                connection.Open();
+
+
+                DataTable dt = new DataTable();
+
+                MySqlDataAdapter da = new MySqlDataAdapter("select * from words", connection);
+
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                connection.Close();
+            }
+            catch (MySqlException me)
+            {
+                MessageBox.Show("ERROR: " + me.Message);
+            }
+        }
+    }
+    
+}
+
+
+
